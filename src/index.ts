485ddb123dd72a93c519d92339868a49087554ad
Stan <lol97554@gmail.com>
2023-12-25 00:31:57 +0800

    chore: remove unused sample function.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,3 @@
 // Import the native module. On web, it will be resolved to ExpoLineLogin.web.ts
 // and on native platforms to ExpoLineLogin.ts
 import ExpoLineLoginModule from "./ExpoLineLoginModule";
-
-export function hello(): string {
-  return ExpoLineLoginModule.hello();
-}
