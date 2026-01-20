80aaade2d8b967b04d52d34c87832dda78aa18ce
Stan <lol97554@gmail.com>
2026-01-20 15:14:06 +0800

    feat: reject promise with error code.

--- ios/ExpoLineLoginModule.swift
+++ ios/ExpoLineLoginModule.swift
@@ -7,9 +7,6 @@ enum BotPromptType: String, Enumerable {
 }
 
 public class ExpoLineLoginModule: Module {
-  // Each module class must implement the definition function. The definition consists of components
-  // that describes the module's functionality and behavior.
-  // See https://docs.expo.dev/modules/module-api for more details about available components.
     public func definition() -> ModuleDefinition {
         Name("ExpoLineLogin")
         
@@ -32,9 +29,12 @@ public class ExpoLineLoginModule: Module {
                             let valueJson = try value.toJSON()
                             promise.resolve(valueJson)
                         } catch {
-                            promise.reject(Exception(name: "JSON Parse Error", description: "Error in Parse to JSON"))
+                            promise.reject("JSON_PARSE_FAILED", "Error in Parse to JSON")
                         }
                     case .failure(let error):
+                        if error.isUserCancelled {
+                            promise.reject("ERR_USER_CANCELLED", "User cancel the login process")
+                        }
                         promise.reject(error)
                     }
                 }
@@ -58,7 +58,7 @@ public class ExpoLineLoginModule: Module {
                         let profileJson = try profile.toJSON()
                         promise.resolve(profileJson)
                     } catch {
-                        promise.reject(Exception(name: "JSON Parse Error", description: "Error in Parse to JSON"))
+                        promise.reject("JSON_PARSE_FAILED", "Error in Parse to JSON")
                     }
                 case .failure(let error):
                     promise.reject(error)
@@ -71,10 +71,10 @@ public class ExpoLineLoginModule: Module {
                 do {
                     promise.resolve(try token.toJSON())
                 } catch {
-                    promise.reject(Exception(name: "JSON Parse Error", description: "Error in Parse to JSON"))
+                    promise.reject("JSON_PARSE_FAILED", "Error in Parse to JSON")
                 }
             } else {
-                promise.reject(Exception(name: "Error", description: "Get AccessToken Error"))
+                promise.reject("GET_TOKEN_FAILED", "Failed to Get AccessToken")
             }
         }
         
